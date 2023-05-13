73d57dbf3521c9e8272195c762c30ec97b1e4121
Vincent Pheng <vpheng@calpoly.edu>
2023-05-13 02:30:59 -0700

    Fix main menu scroll

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,7 @@ var loading_screen = preload("res://scenes/load.tscn")
 
 func _ready():
 	$GodotScreen.visible = true
+	$CreditScreen.visible = false
 
 func _on_start_button_pressed():
 	var loading_screen_instance = loading_screen.instantiate()
@@ -12,8 +13,12 @@ func _on_start_button_pressed():
 
 
 func _on_credits_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/credit_screen.tscn")
+	$CreditScreen.visible = true
 
 
 func _on_animation_player_animation_finished(anim_name):
 	$GodotScreen.queue_free()
+
+
+func _on_back_button_pressed():
+	$CreditScreen.visible = false
